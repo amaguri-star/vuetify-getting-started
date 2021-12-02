d617b7ab2b4463b00fdbc7f446fe92ccfa6d635a
Ryusei <ryusei1597@gmail.com>
2021-12-02 11:54:15 +0900

    first commit

---
+++ src/components/TaskList.vue
@@ -0,0 +1,126 @@
+<template>
+    <v-row>
+        <v-col cols="12" sm="8" offset-sm="2">
+            <v-text-field label="input task you done"></v-text-field>
+            <v-card>
+                <v-list two-line>
+                    <v-list-item v-for="item in items" :key="item.title" link>
+                        <v-list-item-avatar>
+                            <v-icon :class="[item.iconClass]">
+                                {{ item.icon }}
+                            </v-icon>
+                        </v-list-item-avatar>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{
+                                item.title
+                            }}</v-list-item-title>
+
+                            <v-list-item-subtitle>{{
+                                item.subtitle
+                            }}</v-list-item-subtitle>
+                        </v-list-item-content>
+
+                        <v-list-item-action>
+                            <v-btn icon>
+                                <v-icon color="grey lighten-1">
+                                    mdi-information
+                                </v-icon>
+                            </v-btn>
+                        </v-list-item-action>
+                    </v-list-item>
+
+                    <v-list-item v-for="item in items2" :key="item.title" link>
+                        <v-list-item-avatar>
+                            <v-icon :class="[item.iconClass]">
+                                {{ item.icon }}
+                            </v-icon>
+                        </v-list-item-avatar>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{
+                                item.title
+                            }}</v-list-item-title>
+
+                            <v-list-item-subtitle>{{
+                                item.subtitle
+                            }}</v-list-item-subtitle>
+                        </v-list-item-content>
+
+                        <v-list-item-action>
+                            <v-btn icon ripple>
+                                <v-icon color="grey lighten-1">
+                                    mdi-information
+                                </v-icon>
+                            </v-btn>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-list>
+
+                <v-dialog v-model="dialog" max-width="500px">
+                    <v-card>
+                        <v-card-text>
+                            <v-text-field label="File name"></v-text-field>
+
+                            <small class="grey--text"
+                                >* This doesn't actually save.</small
+                            >
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+
+                            <v-btn text color="primary" @click="dialog = false">
+                                Submit
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dialog: false,
+            items: [
+                {
+                    icon: "mdi-folder",
+                    iconClass: "grey lighten-1 white--text",
+                    title: "Photos",
+                    subtitle: "Jan 9, 2014",
+                },
+                {
+                    icon: "mdi-folder",
+                    iconClass: "grey lighten-1 white--text",
+                    title: "Recipes",
+                    subtitle: "Jan 17, 2014",
+                },
+                {
+                    icon: "mdi-folder",
+                    iconClass: "grey lighten-1 white--text",
+                    title: "Work",
+                    subtitle: "Jan 28, 2014",
+                },
+            ],
+            items2: [
+                {
+                    icon: "mdi-clipboard-text",
+                    iconClass: "blue white--text",
+                    title: "Vacation itinerary",
+                    subtitle: "Jan 20, 2014",
+                },
+                {
+                    icon: "mdi-gesture-tap-button",
+                    iconClass: "amber white--text",
+                    title: "Kitchen remodel",
+                    subtitle: "Jan 10, 2014",
+                },
+            ],
+        };
+    },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import HelloWorld from '@/components/HelloWorld'
+import TaskList from '@/components/TaskList'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', component: HelloWorld },
+    { path: '/tasks', component: TaskList },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+})
 
 new Vue({
-  render: h => h(App),
+    vuetify,
+    render: h => h(App),
+    router: router,
 }).$mount('#app')
